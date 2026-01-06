e7ab1009b7c290939f61b518226dc621c91c3ead
Moody-0x01 <azmoudhossin0101@gmail.com>
2026-01-06 15:00:55 +0000

    Done, everything works as expected, tomorrow I will add docs.. also make sure everything works as expected...

--- srcs/requirements/mariadb/mr_setup.sh
+++ srcs/requirements/mariadb/mr_setup.sh
@@ -6,14 +6,16 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     
     mysql_install_db --user=mysql --datadir=/var/lib/mysql
     
-    mysqld --user=mysql &
     pid="$!"
  
     sleep 5
-    
-    mariadb -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-    mariadb -e "CREATE DATABASE ${MYSQL_DATABASE};"
-    
+	mariadb <<-EOSQL
+        ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+        CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;
+        CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+        GRANT ALL PRIVILEGES ON \`${MYSQL_DATABASE}\`.* TO '${MYSQL_USER}'@'%';
+        FLUSH PRIVILEGES;
+	EOSQL
     kill "$pid"
     wait "$pid"
 fi
--- srcs/requirements/wordpress/install.sh
+++ srcs/requirements/wordpress/install.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
+set -e
 cd /var/www/html
-
-if wp core is-installed; then 
-	wp core download --allow-root
+sleep 10
+if [ ! -f wp-includes/version.php ]; then
+    echo "Downloading WordPress..."
+    wp core download --allow-root
+fi
+if [ ! -f wp-config.php ]; then
+	wp config create \
+		--dbname="$WORDPRESS_DB_NAME" \
+		--dbuser="$WORDPRESS_DB_USER" \
+		--dbpass="$WORDPRESS_DB_PASSWORD" \
+		--dbhost="$WORDPRESS_DB_HOST" \
+		--allow-root
+fi
+if ! wp core is-installed --allow-root ; then 
+	wp core install \
+		--allow-root \
+        --url="$URL" \
+        --title="$SITE_TITLE" \
+        --admin_user="$WP_USER" \
+        --admin_password="$WP_PASSWORD" \
+        --admin_email="$EMAIL" \
+		--skip-email \
+        --allow-root
 	chown -R www-data:www-data .
 fi
+
 exec php-fpm8.2 -F
